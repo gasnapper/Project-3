35e6fdcd84ce80d75dc95bb44b7ef7658396cbd6
gasnapper <gasnapper@hotmail.com>
2015-04-03 09:27:51 -0400

    Classes
    Added blank classes for every sort. The sorts will ned to implement or
    extend runnable in preparation for threads on each class.

--- src/Window.java
+++ src/Window.java
@@ -1,10 +1,12 @@
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 
 public class Window {
 	private JFrame frame;
 	private int height;
 	private int width;
+	private JLabel numberLabel;
 	
 	public Window(){
 		height = 500;
