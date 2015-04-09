6abf0da1c572094dba3ae49a091bf295143dce70
plvaloyi89 <plvaloyi@gmail.com>
2015-04-09 16:05:22 -0400

    Thread
    There is a running thread in the Insertion sort class, you can look at
    that to get an idea. Then you can call it in the SortDriver class to run

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,4 +1,25 @@
 
-public class InsertionSort {
+public class InsertionSort implements Runnable {
 
+	Thread t;
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+	
+		System.out.println("The thread is running");
+	}
+
+	
+	public void begin(){
+		
+		System.out.println("Starting");
+		
+		if (t==null){
+			
+			t = new Thread(this);
+			t.start();
+		
+		}
+	}
 }
--- src/SortDriver.java
+++ src/SortDriver.java
@@ -4,6 +4,9 @@ public class SortDriver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Window w = new Window();
+		
+		InsertionSort R1= new InsertionSort();
+		R1.begin();
 	}
 
 }
