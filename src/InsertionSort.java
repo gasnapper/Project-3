8bcf4c56b036ea19a367d2031fac576abc86d56a
plvaloyi89 <plvaloyi@gmail.com>
2015-04-11 17:20:18 -0400

    Insertion sort
    Okay, so insertion sort is working; just gotta sort out the output and
    implement the window to add values in to go into the array

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -6,8 +6,29 @@ public class InsertionSort implements Runnable {
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+	int i ,j ;
+	int key = 0;
+	int[] data= {0,55,42,2,8,18,98,722};
+for( i=1;i< data.length;i++){
+		
+		System.out.print(data[i]);
+}
 	
-		System.out.println("The thread is running");
+	for( i=1;i< data.length;i++){
+		
+		
+		key=data[i];
+		j=i;
+		while(j >0 && data[j-1]>key){
+			
+			data[j]=data[j-1];
+			j--;
+		}
+		
+		data[j]=key;
+	}
+	for(i=0;i<data.length;i++)
+	System.out.print("After Sorting" +data[i] + ",");
 	}
 
 	
