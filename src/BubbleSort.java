e735afcb47f3307fe55297d47466527bae444507
jkastan4600 <jkastan4600@gmail.com>
2015-04-16 16:39:04 -0400

    made it work

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,4 +1,30 @@
-
+import java.util.Arrays;
 public class BubbleSort {
 
+
+//-------------------------------------------------
+private static <Integer extends Comparable<Integer>>
+		void swap(Integer[] data, int index1, int index2)
+	{
+		Integer temp = data[index1];
+		data[index1] = data[index2];
+		data[index2] = temp;
+	}
+
+//--------------------------------------------------
+public static < Integer extends Comparable<Integer>>
+			void bubbleSort(Integer[] data)
+{
+	int position, scan;
+	Integer temp;
+
+	for (position =  data.length - 1; position >= 0; position--)
+		{
+	    	for (scan = 0; scan <= position - 1; scan++)
+	        	{
+	            	if (data[scan].compareTo(data[scan+1]) > 0)
+	                    swap(data, scan, scan + 1);
+	            }
+	        }
+    	}
 }
